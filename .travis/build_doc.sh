729050df711f5aa20677ccf723b2f573b459d094
Kernc <kerncece@gmail.com>
2016-12-12 20:25:07 +0100

    Travis: ensure that docs don't include truecolor images

--- .travis/build_doc.sh
+++ .travis/build_doc.sh
@@ -1,6 +1,16 @@
-cd "$TRAVIS_BUILD_DIR"
+#!/bin/bash
+
+# Ensure images have indexed palettes
+while read image; do
+    if identify -verbose "$image" | grep -q '^ *Type: TrueColor'; then
+        echo "Error: image '$image' is true color" >&2
+        not_ok=1
+    fi
+done < <(find "$TRAVIS_BUILD_DIR/doc" | grep -iP '\.(png|jpg)')
+[ "$not_ok" ] && false
 
 # build Orange inplace (needed for docs to build)
+cd "$TRAVIS_BUILD_DIR"
 python setup.py build_ext --inplace
 
 cd $TRAVIS_BUILD_DIR/doc/development
